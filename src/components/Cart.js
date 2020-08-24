0f31c467763ee075c4ecf38e8c2544d1c601b5bf
Korey <maddenkorey@gmail.com>
2020-08-23 21:04:06 -0400

    fixed lots of bugs

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,19 +5,26 @@ import { Link } from 'react-router-dom';
 class Cart extends Component {
 
     state = {
-        cart: []
+        cart: [],
+        logStatus: false
     };
 
     componentDidMount() {
         fire.auth().onAuthStateChanged(user => {
             if (user) {
                 console.log('logged in on cart page');
-                let uid = user.uid;
-                const pathRef = fire.database().ref("cart").child(uid).child("items");
-                const totalPathRef = fire.database().ref("cart").child(uid).child("total");
-                let total = 0;
+                this.uid = user.uid;
+            } else {
+                console.log('not logged in on cart page');
+                this.uid = 'notsignedin';
+            }
 
-                pathRef.once('child_added', snapshot => {
+            const pathRef = fire.database().ref("cart").child(this.uid).child("items");
+            const totalPathRef = fire.database().ref("cart").child(this.uid).child("total");
+            let total = 0;
+
+            pathRef.on('child_added', snapshot => {
+                if (window.location.pathname === "/cart") {
                     let id = snapshot.key;
                     let image = snapshot.val().image;
                     let name = snapshot.val().name;
@@ -25,6 +32,7 @@ class Cart extends Component {
                     let quantity = snapshot.val().quantity;
                     let subtotal = snapshot.val().subtotal;
 
+                    console.log('stuff: ' + total, snapshot.val().subtotal);
                     total = total + snapshot.val().subtotal;
 
                     totalPathRef.set(total).then(() => {
@@ -33,18 +41,65 @@ class Cart extends Component {
                     });
 
                     let stateCopy = this.state.cart;
-                    let item = {id, image, name, price, quantity, subtotal};
+                    let item = {
+                        id,
+                        image,
+                        name,
+                        price,
+                        quantity,
+                        subtotal
+                    };
                     stateCopy.push(item);
                     this.setState({
                         cart: stateCopy
                     })
-                }).then(() => {console.log("ran cart update once")})
+                } else {
+                    console.warn('Why is this running on the shop page?')
+                }
 
-            } else {
-                console.log('not logged in on cart page')
-            }
+            });
+
+            // See if user is logged in or not
+            fire.auth().onAuthStateChanged(user => {
+                if (user) {
+                    this.setState({
+                        logStatus: true
+                    })
+                } else {
+                    this.setState({
+                        logStatus: false
+                    })
+                }
+                console.log(`user's logStatus: ${this.state.logStatus}`);
+            })
+
+
         });
     }
+
+    removeFromCart = (item) => {
+        // get uid if logged in
+        if (this.state.logStatus) {
+            this.uid = fire.auth().currentUser.uid;
+        } else {
+            const id = 'notsignedin';
+            console.log('removing from cart when not signed in');
+            this.uid = id;
+        }
+
+        let pathRef = fire.database().ref("cart").child(this.uid).child("items").child(item.id);
+
+        pathRef.once('value', snapshot => {
+            let snapSubtotal = snapshot.child("subtotal").val();
+        }).then(e => {console.warn(`removed item(s) from cart`)});
+
+        pathRef.remove().then(() => {
+            console.log("item successfully removed", this.uid);
+            window.location.reload()
+        }).catch(e => {
+            console.log(e.message)
+        });
+    };
 
     render() {
         const cartList = this.state.cart.map(cartItem => {
@@ -60,6 +115,9 @@ class Cart extends Component {
                             </p>
                             <p className="cart-item-id">
                                 Item ID: {cartItem.id}
+                            </p>
+                            <p>
+                                <span id="cart-remove" className="btn transparent z-depth-0 red-text text-darken-2 no-padding" onClick={() => {this.removeFromCart(cartItem)}}>Remove</span>
                             </p>
                         </div>
                         <div className="col s2">
