062bbade930fae8843dd7e5ea0bf93d2e0465d90
Korey Madden <maddenkorey@gmail.com>
2021-03-22 10:24:29 -0400

    moved firebase config to seperate file

--- src/components/Fire.js
+++ src/components/Fire.js
@@ -1,15 +1,7 @@
 import * as firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/database';
-
-const config = {
-    apiKey: "AIzaSyCPs3qjehM9NovrcFDmD51PvQIYhsw34Tc",
-    authDomain: "fragrance-store.firebaseapp.com",
-    databaseURL: "https://fragrance-store.firebaseio.com",
-    projectId: "fragrance-store",
-    storageBucket: "fragrance-store.appspot.com",
-    messagingSenderId: "328926068743"
-};
+import { config } from '../fire-config';
 
 const fire = firebase.initializeApp(config);
 
---
+++ src/fire-config.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyCPs3qjehM9NovrcFDmD51PvQIYhsw34Tc",
+    authDomain: "fragrance-store.firebaseapp.com",
+    databaseURL: "https://fragrance-store.firebaseio.com",
+    projectId: "fragrance-store",
+    storageBucket: "fragrance-store.appspot.com",
+    messagingSenderId: "328926068743"
+};
